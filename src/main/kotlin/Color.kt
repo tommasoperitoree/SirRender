f0d028d9bb866db31945a720b61273c9abfb8575
Tommaso <tommasoperitoree@gmail.com>
2026-03-04 11:38:30 +0100

    first commit

---
+++ src/main/kotlin/Color.kt
@@ -0,0 +1,11 @@
+package org.example
+/** A RGB color
+ *
+ * @param r The level of red
+ * @param g The level of green
+ * @param b The level of blue
+ */
+
+public data class Color(val r: Float = 0.0f, val g: Float = 0.0f, val b: Float = 0.0f) {
+
+}
