110e1948d4dff4cf523c677bb33f044243170c60
Tommaso <tommasoperitoree@gmail.com>
2026-03-04 12:26:24 +0100

    Add plus, times, are_close, are_colors_close functions

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,4 +1,7 @@
 package org.example
+
+import kotlin.math.abs
+
 /** A RGB color
  *
  * @param r The level of red
@@ -6,6 +9,18 @@ package org.example
  * @param b The level of blue
  */
 
+fun are_close (x: Float, y: Float, epsilon: Float = 1e-5f)
+= abs( x - y ) < epsilon
+
 public data class Color(val r: Float = 0.0f, val g: Float = 0.0f, val b: Float = 0.0f) {
+
+    operator fun plus(other: Color) : Color
+    = Color(r + other.r, g + other.g, b + other.b)
+
+    operator fun times(scalar: Float) : Color
+    = Color(r * scalar, g * scalar, b * scalar)
+
+    fun are_colors_close(other: Color)
+    = are_close (r,other.r) && are_close (g,other.g) && are_close (b,other.b)
 
 }
