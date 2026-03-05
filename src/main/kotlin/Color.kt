5c20c909e9258cb63a346a4eb822e8139db2eb00
Tommaso <tommasoperitoree@gmail.com>
2026-03-05 16:37:09 +0100

    Fix Color class declaration and comment

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -2,28 +2,28 @@ package org.example
 
 import kotlin.math.abs
 
-/** A RGB color
+// Checks if two Floats are within epsilon
+fun areClose (x: Float, y: Float, epsilon: Float = 1e-5f) =
+    abs( x - y ) < epsilon
+
+/** An RGB color
  *
  * @param r The level of red
  * @param g The level of green
  * @param b The level of blue
  */
+data class Color(val r: Float = 0.0f, val g: Float = 0.0f, val b: Float = 0.0f) {
 
-fun are_close (x: Float, y: Float, epsilon: Float = 1e-5f)
-= abs( x - y ) < epsilon
+    operator fun plus(other: Color) : Color =
+        Color(r + other.r, g + other.g, b + other.b)
 
-public data class Color(val r: Float = 0.0f, val g: Float = 0.0f, val b: Float = 0.0f) {
+    operator fun times(scalar: Float) : Color =
+        Color(r * scalar, g * scalar, b * scalar)
 
-    operator fun plus(other: Color) : Color
-    = Color(r + other.r, g + other.g, b + other.b)
+    operator fun times(other: Color) : Color =
+        Color(r * other.r, g * other.g, b * other.b)
 
-    operator fun times(scalar: Float) : Color
-    = Color(r * scalar, g * scalar, b * scalar)
-
-    operator fun times(other: Color) : Color
-    = Color(r * other.r, g * other.g, b * other.b)
-
-    fun are_colors_close(other: Color)
-    = are_close (r,other.r) && are_close (g,other.g) && are_close (b,other.b)
+    fun areColorsClose(other: Color) =
+        areClose (r,other.r) && areClose (g,other.g) && areClose (b,other.b)
 
 }
