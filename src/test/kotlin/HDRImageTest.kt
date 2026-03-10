21b893c45f6570b42dd1c119003bd16de86ae139
Giovanni Anselmi <anselmi.gio17@gmail.com>
2026-03-10 22:11:54 +0100

    Add few test in HDRImegeTest

--- src/test/kotlin/HDRImageTest.kt
+++ src/test/kotlin/HDRImageTest.kt
@@ -7,10 +7,27 @@ class HDRImageTest {
 	val height: Int = 10
 	val img = HDRImage(width, height)
 	
+	/*scrivo delle coordinate generiche di altezza e larghezza per utilizzarle nel test della validCoordinates*/
+	var w1: Int = 2
+	var h1: Int = 6
+	
 	@Test
 	fun equals() {
 		assertTrue { img.width == width }
 		assertTrue { img.height == height }
+	}
+	
+	
+	@Test
+	fun validCoordinates(): Unit {
+		assertTrue {w1>=0 && h1>=0}
+		assertTrue{w1<= img.width && h1<= img.height}
+	}
+	
+	@Test
+	fun pixelOffset(): Unit{
+		assertTrue { img.pixelOffset(w1, h1) == h1 * width + w1 }
+		println(img.pixelOffset(w1, h1))
 	}
 	
 	
