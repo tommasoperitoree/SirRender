92b959aa7bd5cdb71f2bb5cd861ce68ff9bccf6d
Chiara Giunta <giuntachiara1@gmail.com>
2026-03-30 11:59:58 +0200

    Finish main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 data class Parameters(
 	val inputFileName: String,
@@ -9,17 +11,26 @@ data class Parameters(
 fun main(args: Array<String>) {
 	
 	try {
-		require(args.size==4){"Usage: [inputFileName], [factor], [gamma], outputFileName]"}
-		val factor= args[1].toFloatOrNull()
+		require(args.size == 4) { "Usage: [inputFileName], [factor], [gamma], outputFileName]" }
+		val factor = args[1].toFloatOrNull()
 			?: throw IllegalArgumentException("Invalid facto: '${args[1]}' must be a float number")
-		val gamma= args[2].toFloatOrNull()
+		val gamma = args[2].toFloatOrNull()
 			?: throw IllegalArgumentException("Invalid facto: '${args[2]}' must be a float number")
 		val params = Parameters(args[0], factor, gamma, args[3])
 		
-		println("Parametri caricati: $params")
-	} catch (e:IllegalArgumentException) {
+		println("Parameters loaded: $params")
+		
+		val img = FileInputStream(params.inputFileName).use { input -> HDRImage.fromPFMStream(input) }
+		img.normalizeImage(factor)
+		img.clampImage()
+		
+		FileOutputStream(params.outputFileName).use { output -> img.writeLDRImage(output, "PNG", gamma) }
+		
+		println("File saved in ${params.outputFileName}")
+	} catch (e: IllegalArgumentException) {
 		println(e.message)
 	}
+	
 }
 
 
