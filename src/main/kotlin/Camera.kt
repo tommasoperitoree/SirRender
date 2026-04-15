bc39dfde956155c501320c393314dbedd3be6600
Giovanni Anselmi <anselmi.gio17@gmail.com>
2026-04-15 12:26:04 +0200

    Add Class Orthogonal

--- src/main/kotlin/Camera.kt
+++ src/main/kotlin/Camera.kt
@@ -1,11 +1,14 @@
-abstract class Camera(){}
+interface Camera{}
 
 class Orthogonal(
-	val a: Float,
+	val aspectRatio: Float,
 	val transformation: Transformation
-): Camera (){
-
-	fun fireRay
+): Camera{
+	fun fireRay(u: Float, v: Float):Ray{
+		val origin=Point(-1.0f,(1.0f-2*u)*aspectRatio,1.0f-2*v)
+		val dir=Vec(1.0f, 0F, 0F)
+		return Ray(origin,dir).transform(transformation)
+	}
 }
 
 
