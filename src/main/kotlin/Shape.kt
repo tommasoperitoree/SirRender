1daaae9abc5446d2a89f894cea39af21ca4f69e1
Giovanni Anselmi <anselmi.gio17@gmail.com>
2026-04-24 17:27:48 +0200

    Add function rayIntersection

--- src/main/kotlin/Shape.kt
+++ src/main/kotlin/Shape.kt
@@ -2,6 +2,9 @@ import kotlin.math.PI
 import kotlin.math.acos
 import kotlin.math.atan2
 
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 interface Shape {
 	// if we need to have shape already have method Transformation, then interface does not work
 	
@@ -12,6 +15,33 @@ interface Shape {
 class Sphere(
 	val transformation: Transformation = Transformation()
 ) {
+	fun rayIntersection(ray: Ray): HitRecord? {
+		val invRay: Ray = ray.transform(transformation.inverse())
+		val o: Vec = ray.origin.toVec()
+		val d: Vec = ray.dir
+		val delta4: Float = (o dot d).pow(2f) - d.squaredNorm().times(o.squaredNorm() - 1f)
+		val t1: Float = (-(o dot d) - sqrt(delta4)) / d.squaredNorm()
+		val t2: Float = (-(o dot d) + sqrt(delta4)) / d.squaredNorm()
+		var tFirstHit = t1
+		
+		tFirstHit = if (t1 > invRay.tMin && t1 < invRay.tMax) {
+			t1
+		} else if (t2 > invRay.tMin && t2 < invRay.tMax) {
+			t2
+		} else {
+			return null
+		}
+		
+		val hitPoint = invRay.at(tFirstHit)
+		return HitRecord(
+			worldPoint = transformation * hitPoint,
+			normal = transformation * sphereNormal(hitPoint, rayDir = ray.dir),
+			spherePointToUV(hitPoint),
+			tFirstHit,
+			ray
+		)
+	}
+	
 	/** Calculation of [Sphere]'s [Normal] at intersection [point] */
 	fun sphereNormal(point: Point, rayDir: Vec): Normal {
 		val result = Normal(point.x, point.y, point.z)
@@ -28,9 +58,6 @@ class Sphere(
 		)
 	}
 	
-	fun rayIntersection(ray: Ray): HitRecord? {
-	
-	}
 }
 
 class Plane(
