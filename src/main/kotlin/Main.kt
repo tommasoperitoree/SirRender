d1857ed7740330bad3da23feaf6d9a2e50e8f81c
Giovanni Anselmi <anselmi.gio17@gmail.com>
2026-05-02 19:41:00 +0200

    Implementation of gif in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,7 +12,13 @@ import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.float
 import com.github.ajalt.clikt.parameters.types.int
 import java.io.FileInputStream
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+import javax.imageio.IIOImage
+import javax.imageio.stream.FileImageOutputStream
+import java.io.File
 import java.io.FileOutputStream
+
 
 // --- Parent command ---
 
@@ -81,6 +87,9 @@ class Demo : CliktCommand(
 	val observerAngle: Int by option(
 		"--observer-angle", "-i",
 	).int().default(30)
+	val numFrames: Int by option(
+		"--num-frames", "-n", help = "Number of frames for the animation"
+	).int().default(36)
 	val outputFileName: String by option(
 		"--output", "-o",
 		help = "Output image file name - extension determines format; default dir ./src/main/resources/"
@@ -102,6 +111,9 @@ class Demo : CliktCommand(
 		val scale = 1 / 10f
 		val scaling = scaling(Vec(scale, scale, scale))
 		val coords = listOf(-0.5f, 0.5f)
+		val frames = mutableListOf<BufferedImage>()
+		val angleStep = 360f / numFrames
+		
 		for (x in coords) {
 			for (y in coords) {
 				for (z in coords) {
@@ -114,35 +126,80 @@ class Demo : CliktCommand(
 		world.addShape(Sphere(translation(Vec(0f, 0f, -0.5f)) * scaling))
 		world.addShape(Sphere(translation(Vec(0f, 0.5f, 0f)) * scaling))
 		
-		val img = HDRImage(width, height)
-		val screenCenter = Vec(-1f, 0f, 0f)
-		val cam = when (camera.lowercase()) {
-			//TODO: generate an animation with a rotation before translation (the first one is on the right),
-			//TODO: use observerAngle
-			
-			"orthogonal" -> OrthogonalCamera(transformation = translation(screenCenter))
-			"perspective" -> PerspectiveCamera(transformation = translation(screenCenter))
-			else -> throw IllegalStateException("No camera  found for $camera.")
-		}
-		val tracer = ImageTracer(img, cam)
-		tracer.fireAllRays { ray -> world.rayIntersection(ray)?.let { white() } ?: black() }
 		
-		img.normalizeImage(factor)
-		img.clampImage()
+		for (frameIndex in 0 until numFrames) {
+			val img = HDRImage(width, height)
+			val currAngle = observerAngle.toFloat() + (frameIndex * angleStep)
+			
+			// Per orbitare, prima trasliamo indietro la camera e poi la ruotiamo
+			//da capire come fare a ruotare la camera per non far si che le sfere si sovrappongano
+			val camTrans = rotationZ(currAngle) * translation(Vec(-2f, 0f, 0.5f))
+			val cam = when (camera.lowercase()) {
+				"orthogonal" -> OrthogonalCamera(transformation = camTrans)
+				//"orthogonal" -> OrthogonalCamera(transformation = translation(screenCenter))
+				"perspective" -> PerspectiveCamera(transformation = camTrans)
+				else -> throw IllegalStateException("No camera  found for $camera.")
+			}
+			val tracer = ImageTracer(img, cam)
+			tracer.fireAllRays { ray -> world.rayIntersection(ray)?.let { white() } ?: black() }
+			
+			img.normalizeImage(factor)
+			img.clampImage()
+			//using this it save all of frame, afterwords use a null stream ByteArrayOutputStream
+			val bImage = img.writeLDRImage(java.io.ByteArrayOutputStream(), "png", gamma)
+			frames.add(bImage)
+		}
+		/*
 		val outputFilePath = "./src/main/resources/$outputFileName"
 		val format = outputFilePath.substringAfterLast(".").lowercase()
 		FileOutputStream(outputFilePath).use { img.writeLDRImage(it, format, gamma) }
 		
 		println("Saved $outputFilePath")
+		*/
+		/**
+		 * Here all the frame are put together to create a gif using [writer]
+		 * For each frame it has to create an object typo [IIOImage] and pass it to [writer]
+		 * In order to see the gif open file directly with app or browser
+		 */
 		
-		// TODO: generate demo image content here
+		val gifFile = File("./src/main/resources/animation.gif")
+		val output = FileImageOutputStream(gifFile)
+		val writer = ImageIO.getImageWritersByFormatName("gif").next()
+		writer.output = output
+		
+		writer.prepareWriteSequence(null) // Start the sequence
+		
+		for (frame in frames) {
+			val imageWriteParam = writer.defaultWriteParam
+			val metadata = writer.getDefaultImageMetadata(
+				javax.imageio.ImageTypeSpecifier.createFromRenderedImage(frame),
+				imageWriteParam
+			)
+			
+			//setting time delay in order to observe the animation
+			val formatName = "javax_imageio_gif_image_1.0"
+			val root = metadata.getAsTree("javax_imageio_gif_image_1.0") as javax.imageio.metadata.IIOMetadataNode
+			val gce =
+				root.getElementsByTagName("GraphicControlExtension").item(0) as? javax.imageio.metadata.IIOMetadataNode
+					?: javax.imageio.metadata.IIOMetadataNode("GraphicControlExtension")
+			gce.setAttribute("delayTime", "100")
+			if (root.getElementsByTagName("GraphicControlExtension").length == 0) {
+				root.appendChild(gce)
+			}
+			
+			// Converte l'albero XML modificato di nuovo nel formato binario dei metadati
+			metadata.setFromTree(formatName, root)
+			
+			// Scrive il frame con i suoi metadati specifici
+			writer.writeToSequence(javax.imageio.IIOImage(frame, null, metadata), imageWriteParam)
+		}
+		writer.endWriteSequence()
 	}
 }
-
 // --- Entry point ---
 
 fun main(args: Array<String>) = SirRender()
 	.subcommands(Pfm2Png(), Demo())
 	.main(args)
 
-// ./gradlew run --args="demo --width=480 --height=480 --output demo.png"
+// ./gradlew run --args="demo --width=480 --height=480 --output demo.png --num-frames=72"
