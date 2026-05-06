3ac6c093c16f728b7073f359647ae1ebcc08712d
Giovanni Anselmi <anselmi.gio17@gmail.com>
2026-05-06 18:54:14 +0200

    Implement UniformPigment

--- src/main/kotlin/Pathtracing.kt
+++ src/main/kotlin/Pathtracing.kt
@@ -2,18 +2,24 @@
  * The [Pigment] type is abstract and represents the color associated with a particular point on a surface ([u],[v])
  */
 interface Pigment {
-	val u: Float
-	val v: Float
-	
 	fun getColor(u: Float, v: Float): Color {
 		throw NotImplementedError("Pigment.getColor($u,$v) is not implemented")
 	}
 }
 
-class uniformPigment() : Pigment {
-
+class uniformPigment(
+	var r: Float = 0f,
+	var g: Float = 0f,
+	var b: Float = 0f
+) : Pigment {
+	override fun getColor(u: Float, v: Float): Color = Color(r, g, b)
 }
 
-class checkeredPigment() : Pigment {
+class checkeredPigment(
+	var r: Float = 0f,
+	var g: Float = 0f,
+	var b: Float = 0f,
+	val n: Int
+) : Pigment {
 
 }
