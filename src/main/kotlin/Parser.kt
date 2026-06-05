a5705c092c5b7f737ec7ecba1c1e494bb1c46929
Giovanni Anselmi <anselmi.gio17@gmail.com>
2026-06-05 09:39:21 +0200

    Structure parese function

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -11,7 +11,7 @@ data class Scene(
 	
 	/** Read a [Token] from [s] and check that it matches the given [symbol]. */
 	fun expectSymbol(s: SceneInputStream, symbol: String) {
-		val token = s.readToken() ?: throw GrammarError(s.location, "unexpected end of file")
+		val token = s.readToken() ?: throw GrammarError(s.location, "unexpected end of file") //analogue to StopToken
 		
 		if (token !is SymbolToken || token.symbol.toString() != symbol) throw GrammarError(
 			token.location,
@@ -54,13 +54,56 @@ data class Scene(
 	
 	fun expectIdentifier(s: SceneInputStream): String {
 		val token = s.readToken() ?: throw GrammarError(s.location, "unexpected end of file")
-	
+		
 		if (token !is IdentifierToken) throw GrammarError(token.location, "got $token instead of identifier")
 		return token.identifier
 	}
 	
 	
 	// --- Parsing functions ---
+	fun parseColor(s: SceneInputStream, scene: Scene): Color {
+		
+		expectSymbol(s, "(")
+		val r = expectNumber(s, scene)
+		expectKeyword(s, ",")
+		val g = expectNumber(s, scene)
+		expectSymbol(s, ",")
+		val b = expectNumber(s, scene)
+		expectSymbol(s, ")")
+		
+		return Color(r, g, b)
+	}
 	
+	fun parseVec(s: SceneInputStream, scene: Scene): Vec {
+		TODO()
+	}
+	
+	fun parsePigment(s: SceneInputStream, secene: Scene): Pigment {
+		TODO()
+	}
+	
+	fun parseBRDF(s: SceneInputStream, scene: Scene): BRDF {
+		TODO()
+	}
+	
+	fun parseMaterial(s: SceneInputStream, scene: Scene): Material {
+		TODO()
+	}
+	
+	fun parseTransformation(s: SceneInputStream, scene: Scene): Transformation {
+		TODO()
+	}
+	
+	fun parseSphere(s: SceneInputStream, scene: Scene): Sphere {
+		TODO()
+	}
+	
+	fun parsePlane(s: SceneInputStream, scene: Scene): Plane {
+		TODO()
+	}
+	
+	fun parseCamera(s:SceneInputStream,scene: Scene):Camera{
+		TODO()
+	}
 	
 }
