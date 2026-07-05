3700a8682631230b9d258a0d3d296d912b94045e
Chiara Giunta <giuntachiara1@gmail.com>
2026-07-05 16:05:46 +0200

    Create PointLightRenderer function with test

--- src/main/kotlin/core/Renderer.kt
+++ src/main/kotlin/core/Renderer.kt
@@ -4,7 +4,9 @@ import geometry.Ray
 import materials.Color
 import math.PCG
 import math.Point
+import kotlin.math.cos
 import kotlin.math.max
+import kotlin.math.pow
 import kotlin.time.measureTimedValue
 import kotlin.time.measureTime
 import kotlin.time.Duration
@@ -147,7 +149,34 @@ class PointLightRenderer(
 ) : Renderer {
 	
 	override operator fun invoke(ray: Ray): Color {
-		return TODO("Provide the return value")
+		val hit = world.rayIntersection(ray) ?: return backgroundColor
+		
+		var result: Color = Color.black
+		
+		for (light in world.lights) {
+			val toLight = light.position - hit.worldPoint
+			//Costruisci lo shadow ray
+			val shadowRay = Ray(origin = hit.worldPoint, dir = toLight.normalize(), tMin = 1e-3f, tMax = toLight.norm())
+			
+			// 3. Se il punto è in ombra continua
+			if (world.rayIntersection(shadowRay) != null) continue
+			
+			// 4. Calcola cos(theta)
+			val cosTheta = maxOf(0f, hit.normal.dot(toLight.normalize()))
+			
+			// 5. Valuta la BRDF
+			val brdf = hit.shape.material.brdf.eval(
+				normal = hit.normal,
+				inDir = toLight.normalize(),
+				outDir = -ray.dir,
+				uv = hit.surfacePoint
+			)
+			// 6. Applica attenuazione
+			val attenuation = light.linearRadius.pow(2) / toLight.norm()
+			
+			result += brdf * light.color * cosTheta * attenuation
+		}
+		return result
 	}
 
 }
