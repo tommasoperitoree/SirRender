4de7516e20a3b3412d097aa909d4a927901d70ab
Chiara Giunta <giuntachiara1@gmail.com>
2026-07-08 15:28:46 +0200

    Modify Shape and World

--- src/main/kotlin/geometry/Shape.kt
+++ src/main/kotlin/geometry/Shape.kt
@@ -1,6 +1,7 @@
 package geometry
 
 import materials.Material
+import math.Point
 import math.Transformation
 
 
@@ -13,7 +14,18 @@ interface Shape {
 	val transformation: Transformation
 	val material: Material
 	
-	/** Compute the intersection between a [ray] and this [Shape] */
+	/** Compute the intersection between a [ray] and this [Shape]. */
 	fun rayIntersection(ray: Ray): HitRecord? =
 		throw NotImplementedError("geometry.Shape.rayIntersection() is abstract")
+	
+	/** Computes all intersections between a [ray] and this [Shape]. */
+	fun rayIntersectionShape(ray: Ray): List<HitRecord> {
+		val hit = rayIntersection(ray)
+		return if (hit != null) listOf(hit) else emptyList()
+	}
+	
+	/** Returns true if [point] is inside the [Shape]. */
+	fun contains(point: Point): Boolean =
+		throw NotImplementedError("geometry.Shape.contains() is abstract")
+	
 }
